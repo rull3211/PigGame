ea8f48e88dcc84e2eba9ecf057f26d5eb514d1f1
rull3211 <54317680+rull3211@users.noreply.github.com>
2020-08-10 20:45:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,17 +12,7 @@ let current = 0;
 let player = 0;
 let scores = [0,0];
 let dice;
-function newGame(){
-    current=0
-    document.querySelector(".player-" + player + "-panel").classList.remove('active');
-    player = Math.floor(Math.random()*2);
-    document.querySelector(".player-" + player + "-panel").classList.add('active');
-    scores = [0,0];
-    document.querySelector("#score-1").textContent = 0;
-    document.querySelector("#score-0").textContent = 0;
-    document.querySelector("#current-1").textContent = 0;
-    document.querySelector("#current-0").textContent = 0;
-}
+
 document.querySelector(".btn-roll").addEventListener("click", function(){
     dice = Math.floor((Math.random()*6)+1);
     document.querySelector(".dice").src = "dice-"+ dice +".png";
